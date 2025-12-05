f5d9b5ab2252fa98f827a81ff1044498591fc8a1
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2025-12-05 15:20:44 -0800

    latest code

--- Platformio_Project/IGEN230_Line_Follower/src/main.cpp
+++ Platformio_Project/IGEN230_Line_Follower/src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 
-const int motorAPWM = 21;
-const int motorA1 = 19;
-const int motorA2 = 32;
-const int motorB1 = 33;
+const int motorAPWM = 19; // Right
+const int motorA1 = 32;
+const int motorA2 = 21;
+const int motorB1 = 33; // Left
 const int motorB2 = 25;
 const int motorBPWM = 26;
 
@@ -12,7 +12,6 @@ const int motorBPWM = 26;
 // echo: D27
 
 int pwm = 255; // up to 255
-float motor_speed_balance = 0.5; // balance between motors A and B (0.0 to 1.0)
 
 const int sensorPins[] = {15, 4, 13, 12, 14};
 
@@ -27,6 +26,7 @@ const int sensor_averaged_values_count = 1;
 
 void forward();
 void backward();
+void turn(int direction); // -1 = left, 1 = right
 void stopMotors();
 void readSensors();
 int readSensor(int sensorPin);
@@ -45,15 +45,32 @@ void setup() {
   pinMode(motorB2, OUTPUT);
   pinMode(motorBPWM, OUTPUT);
 
-  // pinMode(sensorPins[0], INPUT);
-  // pinMode(sensorPins[1], INPUT);
-  // pinMode(sensorPins[2], INPUT);
-  // pinMode(sensorPins[3], INPUT);
-  // pinMode(sensorPins[4], INPUT);
+  // Initialize motor speeds
+  setMotorSpeed();
+
+  pinMode(sensorPins[0], INPUT);
+  pinMode(sensorPins[1], INPUT);
+  pinMode(sensorPins[2], INPUT);
+  pinMode(sensorPins[3], INPUT);
+  pinMode(sensorPins[4], INPUT);
   //characterize_sensors();
 }
 
 void loop() {
+  // forward 1 s, left 1 s, backward 1 s, right 1 s, stop 1 s
+
+  // int delay_ms = 2000;
+  // forward();
+  // delay(delay_ms);
+  // turn(-1);
+  // delay(delay_ms);
+  // backward();
+  // delay(delay_ms);
+  // turn(1);
+  // delay(delay_ms);
+  // stopMotors();
+  // delay(delay_ms);
+
   // read sensors and print all of them in a line eg. "1200 1300 1400 1500 1600"
   
   readSensors();
@@ -64,34 +81,29 @@ void loop() {
   }
   Serial.println();
 
-  sleep(500);
+  delay(500);
 
-  return;
+  //motor_speed_balance = 0.5; // balanced
+  // setMotorSpeed();
+  // forward();
+  // sleep(1000);
+  // return;
 
-  motor_speed_balance = 0.5; // balanced
-  setMotorSpeed();
-  forward();
-  return;
-
-  readSensors();
+  // readSensors();
 
   // If 0 active, turn left, if 4 active, turn right, else forward
 
-  if (sensors_activated[0] == 0) {
-    // leftmost sensor on line, turn left
-    motor_speed_balance = 0.0; // favor motor B
-    setMotorSpeed();
-    forward();
-  } else if (sensors_activated[4] == 0) {
-    // rightmost sensor on line, turn right
-    motor_speed_balance = 1.0; // favor motor A
-    setMotorSpeed();
-    forward();
-  } else {
-    motor_speed_balance = 0.5; // balanced
-    setMotorSpeed();
-    forward();
-  }
+  // if (sensors_activated[0] == 0) {
+  //   // leftmost sensor on line, turn left
+  //   turn(-1);
+  //   forward();
+  // } else if (sensors_activated[4] == 0) {
+  //   // rightmost sensor on line, turn right
+  //   turn(1);
+  //   forward();
+  // } else {
+  //   forward();
+  // }
 }
 
 void characterize_sensors() {
@@ -165,6 +177,22 @@ void backward() {
   digitalWrite(motorB2, HIGH);
 }
 
+void turn(int direction) {
+  if (direction < 0) {
+    // turn left
+    digitalWrite(motorA1, HIGH);
+    digitalWrite(motorA2, LOW);
+    digitalWrite(motorB1, LOW);
+    digitalWrite(motorB2, HIGH);
+  } else {
+    // turn right
+    digitalWrite(motorA1, LOW);
+    digitalWrite(motorA2, HIGH);
+    digitalWrite(motorB1, HIGH);
+    digitalWrite(motorB2, LOW);
+  }
+}
+
 void stopMotors() {
   digitalWrite(motorA1, LOW);
   digitalWrite(motorA2, LOW);
@@ -194,8 +222,8 @@ int readSensor(int sensorIndex) {
 }
 
 void setMotorSpeed() {
-  int motorAPWMValue = pwm * motor_speed_balance;
-  int motorBPWMValue = pwm * (1.0 - motor_speed_balance);
+  int motorAPWMValue = pwm; // ;pwm * motor_speed_balance;
+  int motorBPWMValue = pwm; // pwm * (1.0 - motor_speed_balance);
 
   analogWrite(motorAPWM, motorAPWMValue);
   analogWrite(motorBPWM, motorBPWMValue);
